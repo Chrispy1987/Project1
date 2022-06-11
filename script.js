11135c27ae7039218aca3a26ed59e35976a61889
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-06-11 15:46:52 +1200

    first commit - basic functionality to create input boxes and select a random word from array

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//DOM
+
+
+//Global variables - create an array of arrays. 1st Array for (3) letter words, 2nd Array for (4) letter words etc.
+let testArray = ["cat", "dog", "tip", "bee", "fly", "man", "sin", "pop", "red", "sit", "dot", "van", "rot", "bye", "dye", "sad", "arm"]
+let diffilculty = 3;
+let word = "";
+
+//Create letter boxes based on the diffilculty
+const createLetterBoxes = () => {
+    const words = document.querySelector("#words");
+    for (let i = 0; i < diffilculty; i++) {
+        const newBox = document.createElement("input");
+        newBox.classList = "letters";
+        words.appendChild(newBox);
+    }
+}
+
+const chooseWord = () => {
+    const index = Math.round(Math.random() * testArray.length);
+    word = testArray[index];
+    console.log(word);
+}
+
+//ON FIRST LOAD
+createLetterBoxes();
+chooseWord();
