e54daf2eb9db3288ef64f0cfda45c4e636b4d3e1
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-06-13 08:56:01 +1200

    setup Object structure to store game data + event listeners for focus/blur/keyup

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 //DOM
 const showWrongLetters = document.getElementById("wrongLetters");
+const letters = document.getElementsByClassName("letters");
+const submit = document.getElementById("submit");
+
 
 //Global variables - create an array of arrays. 1st Array for (3) letter words, 2nd Array for (4) letter words etc.
 const wordsByLength = [
@@ -8,10 +11,15 @@ const wordsByLength = [
     four = ["jazz", "high", "jerk", "lamb", "jump", "hazy", "jabs", "foxy", "fuze", "joke", "hope", "pray", "play", "stay", "buzz", "pool", "link", "hint", "junk", "jaws", "jams", "ripe", "hand", "site", "shot", "fort", "mean", "lean", "team", "meat", "seat", "unit", "hurt", "slog"],
     five = ["abuse", "adult", "agent", "beach", "basis", "break", "chain", "brown", "chest", "china", "claim", "class", "dream", "final", "floor", "grass", "glass", "green", "group", "heart", "horse", "hotel", "motor", "mouth", "music", "novel", "nurse", "order", "owner", "panel", "phone", "point", "power", "radio", "scope", "score", "sheet", "shirt", "shift", "shock", "youth", "watch", "water", "whole", "while", "white", "woman", "unity", "union", "uncle", "truth"]
 ];
-let wrongLetters = [];    
-let level = 0;
-let winStreak = 0;
-let word = "";
+//Store core game data in an Object 
+const game = {
+    wrongLetters : [], //store incorrect letters to help player
+    level : 0,         //game diffilculty level
+    winStreak : 0,     //how many games player has won in a row
+    word : "",         //random word that player has to guess
+    interval : 2000   //pause timer between rounds
+}
+
 
 //Clear existing inputs for next level
 const clearAll = () => {
@@ -19,32 +27,65 @@ const clearAll = () => {
     while(words.hasChildNodes()) {
       words.firstChild.remove();
     }
-    wrongLetters = [];
+    game.wrongLetters = [];
     showWrongLetters.textContent = "";
 }
 
 //Create letter input boxes based on the level
-const createLetterBoxes = (level) => {
+const createLetterBoxes = () => {
     const words = document.querySelector("#words");
-    for (let i = 0; i < level + 3; i++) {
+    for (let i = 0; i < game.level + 3; i++) {
         const input = document.createElement("input");
-        input.classList = "letters";
+        input.classList.add("letters");
+        input.classList.add("animate");
         input.maxLength = 1;
+
+        //Input field event listeners to add/remove focus, and auto change focus to next Sibling on key press
+        input.addEventListener("focus", function(e) {
+            e.target.style.border = "solid aqua 3px";
+            e.target.style.outline = "solid yellow 1px";
+            e.target.select();
+        })
+        input.addEventListener("blur", function(e) {
+            e.target.style.border = "solid black 1px";
+            e.target.style.outline = "none";
+        })
+        input.addEventListener("keyup", function(e) {
+            let next = e.target.nextSibling;
+            if (next === null) {
+                submit.focus();
+            } else if(next.disabled === true) {
+                //keep checking next sibling for an available input
+                //otherwise, switch focus to the button
+                while (next !== null && next.disabled === true) {
+                    next = next.nextSibling;                                  
+                }
+                if (next === null) {
+                    submit.focus();
+                } else {
+                    next.focus(); 
+                }
+            } else {
+                next.focus();
+            }
+        });
+
         words.appendChild(input);
+        letters[0].focus();
     }
 }
 
 //choose word from array, depending on level. Eg level 1 = 3 letter words, level 2 = 4 letter words, etc
-const chooseWord = (level) => {
-    const wordIndex = Math.round(Math.random() * wordsByLength[level].length);
-    word = wordsByLength[level][wordIndex];
-    console.log(word);
+const chooseWord = () => {
+    const wordIndex = Math.round(Math.random() * wordsByLength[game.level].length);
+    game.word = wordsByLength[game.level][wordIndex];
+    console.log(game.word);
 }
 
 const checkAnswer = (answer, letters) => {
     //breakdown choices to compare letter-by-letter
     const comparePlayerChoice = answer.split("");
-    const compareGameChoice = word.split("");
+    const compareGameChoice = game.word.split("");
 
     //check for exact match, same position
     for (let i = 0; i < answer.length; i++) {
@@ -62,29 +103,42 @@ const checkAnswer = (answer, letters) => {
         if (compareGameChoice.includes(comparePlayerChoice[i])) {
             letters[i].style.backgroundColor = "orange";
         //display incorrect letters
-        } else if (!wrongLetters.includes(letters[i].value) && (!letters[i].disabled)) {
-                wrongLetters.push(letters[i].value);
-                const display = wrongLetters.join(', ');
+        } else if (!game.wrongLetters.includes(letters[i].value) && (!letters[i].disabled)) {
+                game.wrongLetters.push(letters[i].value);
+                const display = game.wrongLetters.join(', ');
                 showWrongLetters.textContent = `Incorrect letters: ${display}`;
             } 
     }
     
     //if correct word, move to next level of game
-    if (answer === word) {
-        alert("CORRECT");
-        winStreak++;
+    if (answer === game.word) {
+        game.winStreak++;
         //refresh hangman
         //display winstreak on screen
         //history of winning words? side panel?
-        level++;
-        clearAll();
-        chooseWord(level);
-        createLetterBoxes(level);
-    } else {
+        game.level++;
+        showWrongLetters.textContent = "CORRECT!!!"
+        setTimeout( () => {
+            clearAll();
+            chooseWord();
+            createLetterBoxes();
+        }, game.interval);
+     } 
+    //  else {
+    //     let index = 0;
+    //     for (const letter of letters) {
+    //         //console.log(letters[index].disabled);
+    //         if (letters[index].disabled) {
+    //             index += 1;
+    //             letters[index].focus();
+    //         } else {
+    //             letters[index].focus();
+    //         }
+    //     }
         //create array to store different stages of hangman
         //update to next state
         //change border color to red when nearing final chances?
-    }
+    // }
 }
 
 //EVENT LISTENERS
@@ -102,6 +156,7 @@ startGame.addEventListener("click", () => {
     checkAnswer(answer, letters);
 });
 
+
 //ON FIRST LOAD - TESTING
-createLetterBoxes(level);
-chooseWord(level);
+createLetterBoxes(game.level);
+chooseWord(game.level);
